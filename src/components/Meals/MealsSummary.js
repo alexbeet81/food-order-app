33e4162e71d25d60755e2366e603b7063e90b0a7
Alex Pritchard <alexbeet1234@gmail.com>
2021-12-02 18:27:37 +0800

    created Header - Layout - Meals components

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,19 @@
+import classes from './MealsSummary.module.css'
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered To You</h2>
+      <p>
+        Choose your favourite meal from out broad selection of available meals
+        and enjoy a delicious lunch or dinner at home.
+      </p>
+      <p>
+        All our meals are cooked with high-quality ingreidents, just-in-time and
+        of course by experienced chefs!
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
