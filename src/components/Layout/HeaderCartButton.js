2c896f90e69495e0829d2efd8f33fb20d33a5e7f
Alex Pritchard <alexbeet1234@gmail.com>
2021-12-09 13:45:28 +0800

    add and remove items from cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,46 @@
+import { useContext, useEffect, useState } from 'react';
+
 import CartIcon from '../Cart/CanIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
+
+  const numberOfItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+
+  const btnClasses = `${classes.button} ${buttonIsHighlighted ? classes.bump : ''}`
+  
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    setButtonIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setButtonIsHighlighted(false)
+    }, 300);
+
+    return () => {
+      clearTimeout(timer)
+    };
+  }, [items]);
+ 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
       <span className={classes.badge}>
-        3
+        {numberOfItems}
       </span>
     </button>
   )
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,20 +1,91 @@
+import { useReducer } from "react";
+
 import CartContext from "./cart-context";
 
-const addItemToCartHandler = (item) => {};
-
-const removeItemFromCartHandler = (id) => {};
-
-const cartContext = {
+const defaultCartState = {
   items: [],
   totalAmount: 0,
-  addItem: addItemToCartHandler,
-  removeItem: removeItemFromCartHandler 
 };
 
-const CartContext = (props) => {
-  return <CartContext.Provider value={cartContext}>
-    {props.children}
-    </CartContext.Provider>;
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    const exsistingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[exsistingCartItemIndex];
+
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[exsistingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
+  if (action.type === "REMOVE") {
+    const exsistingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[exsistingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter(item => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [ ...state.items ];
+      updatedItems[exsistingCartItemIndex] = updatedItem;
+    };
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    };
+  };
+
+  return defaultCartState;
 };
 
-export default CartContext;
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id})
+  };
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
